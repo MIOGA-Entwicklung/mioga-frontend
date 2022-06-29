3093f2dffa9306a1e7fbdc1a59407577a4926267
moab374a <moab374@hotmail.com>
2022-06-29 20:09:57 +0200

    Only match if mioga is imported

--- src/app/warrengruppen/warrengruppen.component.ts
+++ src/app/warrengruppen/warrengruppen.component.ts
@@ -14,6 +14,8 @@ export class WarrengruppenComponent implements OnInit {
 
   isLoading = false;
 
+  miogaImportedStatus = false;
+
   warrengruppenList: Warrengruppe[] = []
 
   zulieferList: Zulieferer[]
@@ -24,7 +26,7 @@ export class WarrengruppenComponent implements OnInit {
 
   closeResult = ``;
 
-  editWarengruppe : Warrengruppe
+  editWarengruppe: Warrengruppe
 
   constructor(private categoriesService: CategoriesService, private modalService: NgbModal) {
   }
@@ -33,6 +35,7 @@ export class WarrengruppenComponent implements OnInit {
     this.categoriesService.getWarrengruppen().subscribe((receivedData: Warrengruppe[]) => {
       this.warrengruppenList = receivedData
       this.isLoading = false
+      this.getMiogaStatus()
     })
 
   }
@@ -44,10 +47,17 @@ export class WarrengruppenComponent implements OnInit {
     })
   }
 
+  public getMiogaStatus(): void {
+    let mioga = this.warrengruppenList.find(warrengruppe => warrengruppe.name === 'Mioga')
+    this.miogaImportedStatus = mioga?.status
+    console.log(this.miogaImportedStatus)
+  }
+
 
   ngOnInit(): void {
     this.getWarrengruppen()
     this.getLieferanten()
+    this.getMiogaStatus()
     this.isLoading = false
   }
 
@@ -97,18 +107,19 @@ export class WarrengruppenComponent implements OnInit {
   requestBackup() {
     this.categoriesService.requestBackup().subscribe((response: void) => {
       console.log(response)
-      alert( " Back Up is Done in : ./database/backup/")
+      alert(" Back Up is Done in : ./database/backup/")
     })
   }
 
   onImportAllWarengruppen() {
-    this.categoriesService.importAllWarengruppen().subscribe((response : void) => {
+    this.categoriesService.importAllWarengruppen().subscribe((response: void) => {
       console.log(response)
       this.getWarrengruppen()
+      this.getMiogaStatus()
     })
   }
 
-  onUpdateWarengruppe(updateForm : NgForm) {
+  onUpdateWarengruppe(updateForm: NgForm) {
 
     this.editWarengruppe.name = updateForm.value.editWGName
     this.editWarengruppe.zuliefererId = updateForm.value.WGConID
@@ -118,7 +129,6 @@ export class WarrengruppenComponent implements OnInit {
     })
 
   }
-
 
 
   /** Show Models **/
@@ -148,7 +158,7 @@ export class WarrengruppenComponent implements OnInit {
     });
   }
 
-  onEditWarengruppe(editWarengruppeModel: any, warrengruppen: any){
+  onEditWarengruppe(editWarengruppeModel: any, warrengruppen: any) {
 
     this.editWarengruppe = warrengruppen
     this.modalService.open(editWarengruppeModel, {ariaLabelledBy: 'editWarengruppeModalFormHeader'}).result.then((result) => {
@@ -158,7 +168,7 @@ export class WarrengruppenComponent implements OnInit {
     });
   }
 
-   static getDismissReason(reason: any): string {
+  static getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
